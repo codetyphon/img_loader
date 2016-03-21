be26b528e169629e77d5cfd3fecc382368f3af99
codetyphon <codetyphon@sina.com>
2016-03-21 14:52:34 +0800

    Update jquery.imgloader.js

--- jquery.imgloader.js
+++ jquery.imgloader.js
@@ -1,8 +1,6 @@
 /*
-* img.imgloader(low_src='http://7vzsbb.com1.z0.glb.clouddn.com/860cd73265406be979b065a985c9a6ec.jpg?imageMogr/v2/thumbnail/50x50',
- middle_src='http://7vzsbb.com1.z0.glb.clouddn.com/860cd73265406be979b065a985c9a6ec.jpg?imageMogr/v2/thumbnail/100x100',
- high_src='http://7vzsbb.com1.z0.glb.clouddn.com/860cd73265406be979b065a985c9a6ec.jpg?imageMogr/v2/thumbnail/600x600')
-* */
+https://github.com/codetyphon/imgloader
+*/
 ;(function($){
     $.fn.extend({
         imgloader:function(){
